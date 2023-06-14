9d83291e4f95fb0a4336525e08418e31b130f6f3
Thorsten Hell <thorsten.hell@cismet.de>
2023-06-14 13:43:14 +0200

    export the help comps

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,7 @@
-import Help05Introduction from ".help/Help05Introduction";
-import Help20Karteninhalt from ".help/Help20Karteninhalt";
-import Help80ModellfehlerMelden from ".help/Help80ModellfehlerMelden";
-import Help90Haftungsausschluss from ".help/Help90Haftungsausschluss";
+import Help05Introduction from "./help/Help05Introduction";
+import Help20Karteninhalt from "./help/Help20Karteninhalt";
+import Help80ModellfehlerMelden from "./help/Help80ModellfehlerMelden";
+import Help90Haftungsausschluss from "./help/Help90Haftungsausschluss";
 import Help99Footer from ".help/Help99Footer";
 
 export {
